100f3c7945ae11f80138fa6111871f2e8ab5c7ee
dev chaico <dev.chaico@gmail.com>
2022-07-15 15:28:07 -0500

    add switch order and readme

--- src/pages/OderNamesPage.tsx
+++ src/pages/OderNamesPage.tsx
@@ -1,11 +1,36 @@
+import { Row, Switch, Text } from "@nextui-org/react";
+import { useState } from "react";
 import { ListNicknames, MainLayout } from "../components";
 
-const names = ["Triviño TI", "Homer Dev", " ", " Ragnar Front", "Loki UX"];
+const names = [
+  "Triviño TI",
+  "Homer Dev",
+  " ",
+  " Ragnar Front",
+  "Loki UX",
+  "Anonymous",
+];
 
 export const OderNamesPage = () => {
+  const [isDesc, setIsDesc] = useState<boolean>(false);
   return (
     <MainLayout title="Listado de Personales">
-      <ListNicknames names={names} order="ASC" />
+      <div>
+        <Row>
+          <Switch
+            squared
+            color="primary"
+            checked={true}
+            onChange={(e) => {
+              setIsDesc((status) => !status);
+            }}
+          />
+          <Text size={18} css={{ marginLeft: "$4" }}>
+            {isDesc ? "DESC" : "ASC"}
+          </Text>
+        </Row>
+        <ListNicknames names={names} order={isDesc ? "DESC" : "ASC"} />
+      </div>
     </MainLayout>
   );
 };
